b31b26fbddd562870930b827673bc795bb8fa515
liujun <liujun@magictavern.com>
2026-01-09 22:28:57 +0800

    feat(core): implement zero-allocation logging using ZString

--- src/Match3.Web/Match3.Web/Services/MicrosoftGameLogger.cs
+++ src/Match3.Web/Match3.Web/Services/MicrosoftGameLogger.cs
@@ -13,15 +13,13 @@ public class MicrosoftGameLogger : IGameLogger
         _logger = logger;
     }
 
-    public void LogInfo(string message)
-    {
-        _logger.LogInformation(message);
-    }
+    public void LogInfo(string message) => _logger.LogInformation(message);
+    public void LogInfo<T>(string template, T arg1) => _logger.LogInformation(template, arg1);
+    public void LogInfo<T1, T2>(string template, T1 arg1, T2 arg2) => _logger.LogInformation(template, arg1, arg2);
+    public void LogInfo<T1, T2, T3>(string template, T1 arg1, T2 arg2, T3 arg3) => _logger.LogInformation(template, arg1, arg2, arg3);
 
-    public void LogWarning(string message)
-    {
-        _logger.LogWarning(message);
-    }
+    public void LogWarning(string message) => _logger.LogWarning(message);
+    public void LogWarning<T>(string template, T arg1) => _logger.LogWarning(template, arg1);
 
     public void LogError(string message, Exception? ex = null)
     {
